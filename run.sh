b33b3d7151962036a733e8d787aa7bb9d20378a9
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2017-10-31 08:20:32 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# format of the arguments is source_file input_file_path output_zip_file_path output_dt_file_path
+python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
